217b1ac4f36203e4885b6d9ac21a4e393f367c13
Alessandro Astone <ales.astone@gmail.com>
2018-11-22 19:26:26 +0100

    get rid of tabs

--- gameoff-2018/Assets/Scripts/UI/MainMenu.cs
+++ gameoff-2018/Assets/Scripts/UI/MainMenu.cs
@@ -5,9 +5,9 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
-	
+
     // Use this function in the editor
-	public void DidPressStartButton() {
+    public void DidPressStartButton() {
         Debug.Log("Loading Level #1");
         SceneManager.LoadScene(1);
     }
