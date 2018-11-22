6a3cae3dcabd57c3def11f18af5e051e4100398d
Andrea Franchini <f.andrea1602@gmail.com>
2018-11-22 18:56:07 +0100

    Added Main Menu

---
+++ gameoff-2018/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+	
+    // Use this function in the editor
+	public void DidPressStartButton() {
+        Debug.Log("Loading Level #1");
+        SceneManager.LoadScene(1);
+    }
+}
