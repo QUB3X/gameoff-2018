d96aa2a5a241dfcaea4a6d25b0ccaf952888c1cb
Andrea Franchini <f.andrea1602@gmail.com>
2018-11-24 15:53:48 +0100

    Added dialogue interface
    TODO: Animating it + Sourcing external text file to display.

---
+++ gameoff-2018/Assets/Scripts/UI/Dialog.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialog : MonoBehaviour {
+
+  // Set true to show the dialog box
+  private bool showDialog = false;
+
+  // The box containing text
+	public GameObject textContainer;
+
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+    //textContainer.SetActive(false);
+	}
+	
+  void Show() {
+    textContainer.SetActive(true);
+  }
+
+  void Hide() {
+    textContainer.SetActive(false);
+  }
+
+  public void Print(string _text) {
+    text.text = _text;
+  }
+}
